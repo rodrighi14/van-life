0d677bc3e10a48e26ff64c9e7f65516004bb9d71
rodrighi14 <rodrim@areaweb14.com>
2024-04-24 14:30:49 -0600

    .

--- index.jsx
+++ index.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from './pages/Vans';
-import VanDetail from './pages/VansDetail';
+import VanDetail from './pages/VanDetail';
 
 import './server'
 

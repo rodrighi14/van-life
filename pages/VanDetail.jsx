892372144bffbf3435b86cc39ff865199f7bd245
rodrighi14 <rodrim@areaweb14.com>
2024-04-24 15:00:45 -0600

    added css vandetail

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -1,9 +1,29 @@
 import React from "react";
-import {useParams} from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { useParams } from 'react-router-dom';
 
 export default function VanDetail() {
+    const [details, setDetails]= useState([]);
     const params = useParams();
-    console.log(params)
 
-    return <h1>Van detail page goes here</h1>
+    useEffect(() => {
+        fetch(`api/vans/${params.id}`)
+            .then(res => res.json())
+            .then(data => setDetails(data.vans))
+    }, [params.id]);
+
+    return (
+        <div className="van-detail-container">
+            {van ? (
+                <div className="van-detail">
+                    <img src={van.imageUrl} />
+                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                    <h2>{van.name}</h2>
+                    <p className="van-price"><span>${van.price}</span>/day</p>
+                    <p>{van.description}</p>
+                    <button className="link-button">Rent this van</button>
+                </div>
+            ) : <h2>Loading...</h2>}
+        </div>
+    )
 }

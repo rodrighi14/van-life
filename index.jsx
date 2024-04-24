e913ffb63fd5ffe72c3313b6f2282093a181bd43
rodrighi14 <rodrim@areaweb14.com>
2024-04-24 14:19:42 -0600

    Added VanDetail

--- index.jsx
+++ index.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from './pages/Vans';
+import VanDetail from './pages/VansDetail';
+
 import './server'
 
 function App() {
@@ -21,7 +23,8 @@ function App() {
       <Routes>
         <Route path="/" element={ <Home /> } />
         <Route path="/about" element={ <About /> } />
-        <Route path="/vans" element={ <Vans />} />
+        <Route path="/vans" element={ <Vans />}/>
+        <Route path="/vans:id" element={ <VanDetail /> }/>
       </Routes>
     </BrowserRouter>
   )
--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -25,6 +25,7 @@ export default function Vans() {
 
     return (
         <div className="van-list-container">
+            <h1>Explore our van options</h1>
             <div className="van-list">
                 {vanElements}
             </div>

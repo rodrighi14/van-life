660d264cdaf63efb647cc66c856fa9634154d547
rodrighi14 <rodrim@areaweb14.com>
2024-04-24 13:15:53 -0600

    add server.js

--- index.jsx
+++ index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import './server'
 
 function App() {
   return (

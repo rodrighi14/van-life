315687df5c9143e7ae3bbaa9c410e70f48c90e46
rodrighi14 <rodrim@areaweb14.com>
2024-04-24 14:47:28 -0600

    .

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -13,8 +13,8 @@ export default function Vans() {
 
     const vanElements = vans.map(van => (
         <div key={van.id} className="van-tile">
-            <Link to={`/vans/${van.id}`}>
-                <img src={van.imageUrl} />
+            <Link to={`/vans/${van.id}`} aria-label={`View details for ${van.name}, priced at $${van.price} per day`}>
+                <img src={van.imageUrl} alt={`Image of ${van.name}`}/>
                 <div className="van-info">
                     <h3>{van.name}</h3>
                     <p>${van.price}<span>/day</span></p>

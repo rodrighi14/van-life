dae437d14ec250379248bde01d2cc8268f127d9e
rodrighi14 <rodrim@areaweb14.com>
2024-04-25 14:14:11 -0600

    .

--- components/Host.jsx
+++ components/Host.jsx
@@ -3,10 +3,10 @@ import {Link} from 'react-router-dom';
 
 export default function Host(){
      return (
-        <nav>
-        <Link to="host">Dashboard</Link>
-        <Link to="host/income">Income</Link>
-        <Link to="host/reviews">Reviews</Link>
+        <nav className="host-nav">
+            <Link to="/host">Dashboard</Link>
+            <Link to="/host/income">Income</Link>
+            <Link to="/host/reviews">Reviews</Link>
         </nav>
      )
 }

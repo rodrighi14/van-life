452257ce68d94b307d26fc86ad10be03a7adbce8
rodrighi14 <rodrim@areaweb14.com>
2024-04-25 15:08:25 -0600

    Update HostLayout.jsx

--- components/HostLayout.jsx
+++ components/HostLayout.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-import {Link, Outlet} from 'react-router-dom';
+import {Link, Outlet, NavLink} from 'react-router-dom';
 
 export default function HostLayout(){
+    const style = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+      }
+
      return (
         <>
         <nav className="host-nav">
-            <Link to="/host">Dashboard</Link>
-            <Link to="/host/income">Income</Link>
-            <Link to="/host/reviews">Reviews</Link>
+            <NavLink style={({isActive}) => isActive ? style : null} to="/host">Dashboard</NavLink>
+            <NavLink style={({isActive}) => isActive ? style : null} to="/host/income">Income</NavLink>
+            <NavLink style={({isActive}) => isActive ? style : null} to="/host/reviews">Reviews</NavLink>
         </nav>
         <Outlet/>
         </>

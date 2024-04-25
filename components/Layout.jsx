5497ba2a46a88fc7643dbf448c3cc89d2b7a0c71
rodrighi14 <rodrim@areaweb14.com>
2024-04-25 01:31:16 -0600

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
+import Header from "./Header";
 
 export default function Layout() {
     return (
         <>
-        <h1>This is the Layout Route</h1>
-        <Outlet/>
+        <Header />
+        <Outlet />
         </>
     )
     

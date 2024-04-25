203a677a54f66114c8d81a80002f5ce933ee6a3d
rodrighi14 <rodrim@areaweb14.com>
2024-04-25 15:02:09 -0600

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import { Link, NavLink } from 'react-router-dom';
 
 export default function Header() {
+  const style = {
+    fontWeight: "bold",
+    textDecoration: "underline"
+    color: "#161616"
+  }
+
     return (
         <header>
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
-          <NavLink to="/host">Host</NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/vans">Vans</NavLink>
+          <NavLink style={({isActive}) => isActive ? style : null} to="/host">Host</NavLink>
+          <NavLink style={({isActive}) => isActive ? style : null} to="/about">About</NavLink>
+          <NavLink style={({isActive}) => isActive ? style : null} to="/vans">Vans</NavLink>
         </nav>
       </header>
     )

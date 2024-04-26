98441727806d95bd48b720fcf72c8d0915e78ffb
rodrighi14 <rodrim@areaweb14.com>
2024-04-26 01:46:02 -0600

    ,

--- pages/Host/HostVans.jsx
+++ pages/Host/HostVans.jsx
@@ -1,27 +1,47 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import React from "react"
+import { Link } from "react-router-dom"
+import { useState, useEffect } from "react";
 
 export default function HostVans() {
-    const [vans, setVans] = useState([]);
+    const [vans, setVans] = useState([])
 
     useEffect(() => {
-        fetch(api/host/vans)
-            .then(res => res.json)
+        fetch("/api/host/vans")
+            .then(res => res.json())
             .then(data => setVans(data.vans))
     }, [])
 
-    const VanElements = vans.map((van) => {
-        <div key={van.id}>
-            <img src={van.imageUrl} aria-label={`View details for ${van.name}, priced at $${van.price} per day`}/>
-            <h3>{van.name}</h3>
-            <p>{van.price}</p>
-        </div>
-    })
+    const hostVansEls = vans.map(van => (
+        <Link
+            to={`/host/vans/${van.id}`}
+            key={van.id}
+            className="host-van-link-wrapper"
+        >
+            <div className="host-van-single" key={van.id}>
+                <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+                <div className="host-van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}/day</p>
+                </div>
+            </div>
+        </Link>
+    ))
 
     return (
-        <div>
-        <h1>Your listed vans</h1>
-        {VanElements}
-        </div>
+        <section>
+            <h1 className="host-vans-title">Your listed vans</h1>
+            <div className="host-vans-list">
+                {
+                    vans.length > 0 ? (
+                        <section>
+                            {hostVansEls}
+                        </section>
+
+                    ) : (
+                            <h2>Loading...</h2>
+                        )
+                }
+            </div>
+        </section>
     )
 }

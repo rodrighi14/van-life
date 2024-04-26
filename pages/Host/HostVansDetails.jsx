33a347066ba0ff1e25929d4199c183d331b5530b
rodrighi14 <rodrim@areaweb14.com>
2024-04-26 02:11:56 -0600

    .

--- pages/Host/HostVansDetails.jsx
+++ pages/Host/HostVansDetails.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import {Link} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 export default function HostVansDetails() {
+    const {id} = useParams()
+    const [currentVan, setCurrentVan] = useState(null)
+
+    useEffect(() => {
+        fetch(`/api/host/vans/${id}`)
+            .then(res => res.json())
+            .then(data => setCurrentVan(data.vans))
+    }, [id])
+
+    if (!currentVan) {
+        return <h1>Loading...</h1>
+    }
+
     return (
-        <h1>HostVansDetails</h1>
+        <section>
+            <div className="host-van-detail-layout-container">
+                <div className="host-van-detail">
+                    <img src={currentVan.imageUrl} />
+                    <div className="host-van-detail-info-text">
+                        <i
+                            className={`van-type van-type-${currentVan.type}`}
+                        >
+                            {currentVan.type}
+                        </i>
+                        <h3>{currentVan.name}</h3>
+                        <h4>${currentVan.price}/day</h4>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
